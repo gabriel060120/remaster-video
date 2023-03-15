34820eb7c74c68dcb89e97e5e4205c1e320d522c
Luan <delpretob13@hotmail.com>
2023-03-15 16:09:12 -0400

    Integração C/Python do módulo `upscalling`em Python com a função `main` em C;

--- src/upscale_main.c
+++ src/upscale_main.c
@@ -9,38 +9,41 @@ void call_from_python(PyObject *module, const char *restrict funcname, char *res
     PyGILState_STATE state = PyGILState_Ensure();
 
     PyObject* fn     = PyObject_GetAttrString(module, funcname);
-    PyObject* args   = Py_BuildValue("(ss)", infname, outfname);
-    PyObject *result = PyObject_CallObject(fn, args);
-    Py_DECREF(args);
-
     if (!PyCallable_Check(fn)) {
         fprintf(stderr, "%s: função esperada\n", __func__);
         goto ERROR_CLEANUP;
     }
+
+    PyObject* args   = Py_BuildValue("(ss)", infname, outfname);
+    PyObject *result = PyObject_CallObject(fn, args);
+    Py_DECREF(args);
 
     if (PyErr_Occurred()) {
         PyErr_Print();
         goto ERROR_CLEANUP;
     }
 
-    if (!PyUnicode_Check(result)) {
-        fprintf(stderr, "%s: string esperada\n", __func__);
+    if (!PyLong_Check(result)) {
+        fprintf(stderr, "%s: valor inteiro de retorno esperado\n", __func__);
         goto ERROR_CLEANUP;
     }
 
-    PyObject *bytes = PyUnicode_AsEncodedString(result, "UTF-8", "strict");
-    if (!bytes) {
-        PyErr_Print();
-        goto ERROR_CLEANUP;
-    }
-    char *res = PyBytes_AS_STRING(bytes);
-    res = strdup(res);
+    /* PyObject *bytes = PyUnicode_AsEncodedString(result, "UTF-8", "strict"); */
+    /* if (!bytes) { */
+    /*     PyErr_Print(); */
+    /*     goto ERROR_CLEANUP; */
+    /* } */
+    /* char *res = PyBytes_AS_STRING(bytes); */
+    /* res = strdup(res); */
 
-    assert(!strcmp(res, "MOCK"));
+    /* assert(!strcmp(res, "MOCK")); */
+    long res = PyLong_AsLong(result);
+    assert(!res);
+
     Py_DECREF(result);
 
     PyGILState_Release(state);
-    free(res);
+    //free(res);
 
 ERROR_CLEANUP:
     Py_XDECREF(result);
@@ -58,7 +61,7 @@ int main(int argc, char *argv[])
     setenv("PYTHONPATH", ".", 1);
     Py_Initialize();
 
-    PyObject *modulename = PyUnicode_FromString((char *) "mock-module.mock_upscale");
+    PyObject *modulename = PyUnicode_FromString((char *) "upscalling.upscalling");
     PyObject *module     = PyImport_Import(modulename);
     Py_DECREF(modulename);
     if (!module) {
@@ -66,7 +69,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    call_from_python(module, "upscale", "/ex/emp/lo", "nome");
+    call_from_python(module, "upscalling", argv[1], argv[2]);
 
     Py_Finalize();
     return EXIT_SUCCESS;
